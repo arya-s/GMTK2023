56ef08968f8a4fde32b448102a56e81c9393409b
arya-s <andrei.borza@gmail.com>
2023-07-09 11:23:14 +0900

    Meh

---
+++ src/Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_fullscreen"):
+		var mode = DisplayServer.window_get_mode()
+		if mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
---
+++ src/Player/player.gd
@@ -0,0 +1,118 @@
+extends CharacterBody3D
+
+const pixelToMeters = 12.0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var AIR_MULTIPLIER: float = 0.65
+@export var GRAVITY: int = -900 / pixelToMeters
+@export var HALF_GRAVITY_THRESHOLD: int = 120 / pixelToMeters
+@export var RUN_ACCELERATION = 1000 / pixelToMeters
+@export var RUN_FRICTION = 400 / pixelToMeters
+@export var RUN_MAX_SPEED = 90 / pixelToMeters
+@export var DUCK_FRICTION = 500 / pixelToMeters
+@export var JUMP_FORCE = 105 / pixelToMeters
+@export var JUMP_HORIZONTAL_BOOST = 40 / pixelToMeters
+@export var WALL_JUMP_HORIZONTAL_BOOST = RUN_MAX_SPEED + JUMP_HORIZONTAL_BOOST
+@export var WALL_SLIDE_START_MAX = 20
+@export var WALL_CHECK_DISTANCE = 3
+@export var FALL_MAX_SPEED = 160 / pixelToMeters
+@export var FALL_MAX_ACCELERATION = 300 / pixelToMeters
+@export var AUTO_JUMP_TIMER = 0.1
+@export var UPWARD_CORNER_CORRECTION = 4
+@export var CEILING_VARIABLE_JUMP = 0.05
+@export var FAST_FALL_MAX_SPEED = 240 / pixelToMeters
+@export var FAST_FALL_MAX_ACCELERATION = 300 / pixelToMeters
+
+var max_fall = FALL_MAX_SPEED
+var variable_jump_speed = 0
+
+var mouse_sensitivity = 0.05
+
+@onready var model = $Visuals
+@onready var spring_arm = $CameraMount/SpringArm
+@onready var variable_jump_timer = $VariableJumpTimer
+@onready var jump_buffer_timer = $JumpBufferTimer
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var camera_mount = $CameraMount
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		# player model should not rotate with mouse
+		model.rotate_y(deg_to_rad(event.relative.x * mouse_sensitivity))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var input_vector := Vector3.ZERO
+#	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+#	input_vector.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+#	input_vector = input_vector.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
+	handle_joystick_look()
+	
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		model.look_at(position + direction)
+	
+	# apply horizontal force
+	var run_multiplier = 1.0 if is_on_floor() else AIR_MULTIPLIER
+	if abs(velocity.x) > RUN_MAX_SPEED and sign(velocity.x) == direction.x:
+		velocity.x = move_toward(velocity.x, direction.x * RUN_MAX_SPEED, RUN_FRICTION * run_multiplier * delta)
+	else:
+		velocity.x = move_toward(velocity.x, direction.x * RUN_MAX_SPEED, RUN_ACCELERATION * run_multiplier * delta)
+		
+	if abs(velocity.z) > RUN_MAX_SPEED and sign(velocity.z) == direction.z:
+		velocity.z = move_toward(velocity.z, direction.z * RUN_MAX_SPEED, RUN_FRICTION * run_multiplier * delta)
+	else:
+		velocity.z = move_toward(velocity.z, direction.z * RUN_MAX_SPEED, RUN_ACCELERATION * run_multiplier * delta)
+	
+	# apply vertical force
+	var fm = FALL_MAX_SPEED
+	max_fall = move_toward(max_fall, fm, FAST_FALL_MAX_ACCELERATION * delta)
+	
+	if not is_on_floor():
+		var fall_multiplier = 0.5 if abs(velocity.y) < HALF_GRAVITY_THRESHOLD and Input.is_action_pressed("jump") else 1.0
+		velocity.y = move_toward(velocity.y, max_fall, GRAVITY * fall_multiplier * delta)
+		
+	if variable_jump_timer.time_left > 0:
+		if Input.is_action_pressed("jump"):
+			velocity.y = min(velocity.y, variable_jump_speed)
+		else:
+			variable_jump_timer.stop()
+			
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or coyote_jump_timer.time_left > 0:
+			jump(direction)
+		else:
+			jump_buffer_timer.start()
+
+	move_and_slide()
+	
+#	if velocity.length() > 0.2:
+#		var look_direction = Vector2(velocity.z, velocity.x)
+#		model.rotation.y = look_direction.angle()
+
+func handle_joystick_look():
+	var input_dir = Input.get_vector("ui_look_left", "ui_look_right", "ui_look_up", "ui_look_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	rotate_y(deg_to_rad(-direction.x * 4))
+	model.rotate_y(deg_to_rad(direction.x * 4))
+	camera_mount.rotate_x(deg_to_rad(-direction.z * 4))
+		
+func jump(input_vector: Vector3):
+	coyote_jump_timer.stop()
+	variable_jump_timer.start()
+	
+#	velocity.x += input_vector.x * JUMP_HORIZONTAL_BOOST
+#	velocity.z += input_vector.z * JUMP_HORIZONTAL_BOOST
+	velocity.y = JUMP_FORCE
+	variable_jump_speed = velocity.y
